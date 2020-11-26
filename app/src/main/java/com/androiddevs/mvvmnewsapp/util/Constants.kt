c1cd94de4586067042cc8a82e047c3373c325284
SaiChand440 <59303987+SaiChand440@users.noreply.github.com>
2020-11-26 16:35:16 +0530

    Setup Database

---
+++ app/src/main/java/com/androiddevs/mvvmnewsapp/util/Constants.kt
@@ -0,0 +1,8 @@
+package com.androiddevs.mvvmnewsapp.util
+
+class Constants {
+    companion object {
+        const val API_KEY = "812fe77e7b974e0498a9d17eb9505875"
+        const val BASE_URL = "https://newsapi.org"
+    }
+}
